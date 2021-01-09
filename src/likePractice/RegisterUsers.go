dd27cb82175df6bba462fa1d696fc40b76e6a8a6
joshua <su@joshua.com>
2021-01-09 15:55:48 +0800

    2021-1-9

--- src/likePractice/RegisterUsers.go
+++ src/likePractice/RegisterUsers.go
@@ -25,27 +25,57 @@ func RegisterUsers(c *gin.Context) {
 	if err := c.ShouldBindJSON(&u); err != nil {
 		fmt.Println(err.Error())
 		Data["code"] = 401
-		Data["message"] = "类型错误！"
+		Data["message"] = "未知错误，请仔细检查！"
 		c.JSON(http.StatusUnauthorized, gin.H{
 			"data": Data,
 		})
 	} else {
-		if u.Username != "" && u.Password != "" {
-			res := registerUser(u.Username, u.Password, time.Now(), time.Now())
-			if res.Error != nil {
-				panic(res.Error)
+		v := TestType(u.Username, u.Password)
+
+		if v {
+			if u.Username != "" && u.Password != "" {
+				res := registerUser(u.Username, u.Password, time.Now(), time.Now())
+				if res.Error != nil {
+					panic(res.Error)
+				}
+				Data["code"] = 200
+				Data["message"] = "注册成功!"
+				c.JSON(http.StatusOK, gin.H{
+					"data":   Data,
+					"output": v,
+				})
+			} else {
+				Data["code"] = 401
+				Data["message"] = "注册失败!"
+				c.JSON(http.StatusUnauthorized, gin.H{
+					"data": Data,
+				})
 			}
-			Data["code"] = 200
-			Data["message"] = "注册成功!"
-			c.JSON(http.StatusOK, gin.H{
-				"data": Data,
-			})
 		} else {
 			Data["code"] = 401
-			Data["message"] = "注册失败!"
+			Data["message"] = "类型错误!"
 			c.JSON(http.StatusUnauthorized, gin.H{
-				"data": Data,
+				"data":   Data,
+				"output": v,
 			})
 		}
+
 	}
+}
+func TestType(items ...interface{}) (b bool) {
+
+	for _, v := range items {
+
+		switch v.(type) {
+
+		case string:
+
+			b = true
+
+		default:
+			b = false
+
+		}
+	}
+	return
 }
